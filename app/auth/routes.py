6fb724dac52dfdd18796b66ac199d69e7b99c1c2
asleybach <asleybach@gmail.com>
2025-04-27 09:54:42 -0300

    ajuste de api de registro | envio token en registro y username en login

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -24,19 +24,35 @@ def login(request: LoginRequest, db: Session = Depends(get_db)):
         raise HTTPException(status_code=401, detail="Invalid email or password")
     
     access_token = create_access_token({"sub": user.email})
-    return TokenResponse(access_token=access_token)
+    
+    return TokenResponse(
+        access_token=access_token,
+        token_type="bearer",
+        username=user.username
+    )
 
-@auth_router.post("/register")
+@auth_router.post("/register", response_model=TokenResponse)
 def register(request: RegisterRequest, db: Session = Depends(get_db)):
     
     user = db.query(User).filter((User.username == request.username) | (User.email == request.email)).first()
     if user:
         raise HTTPException(status_code=400, detail="Username or email already exists")
     
+    
     hashed_password = bcrypt.hashpw(request.password.encode('utf-8'), bcrypt.gensalt())
 
+    
     new_user = User(username=request.username, email=request.email, password=hashed_password.decode('utf-8'))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
-    return {"message": "User registered successfully"}
+
+    
+    access_token = create_access_token({"sub": new_user.email})
+
+    
+    return TokenResponse(
+        access_token=access_token,
+        token_type="bearer",
+        username=new_user.username
+    )
