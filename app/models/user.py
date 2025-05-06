e064eedda1a0ca8df8ff5003bd309f116f9b65d8
asleybach <asleybach@gmail.com>
2025-05-06 09:59:23 -0300

    modelo para ingresos por usuario

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
 from app.database.database import Base
 
 class User(Base):
@@ -8,8 +9,11 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=False)  # Campo de correo electrónico
+    email = Column(String, unique=True, index=True, nullable=False)  
     password = Column(String, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
+
+   
+    incomes = relationship("Income", back_populates="user", cascade="all, delete-orphan")
 
